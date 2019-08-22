71e527e4928f0906c3a0a25088f08034d13008a2
Keenan McDonald <keemcd@gmail.com>
2019-08-21 20:11:14 -0500

    final version, assignment 1

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let headline = $('.headline');
 let stickyPosition = myName.offset().top - parseInt(myName.css('marginTop')); //accounting for the upper margin
 let headlineOffset = headline.offset().top - myName.offset().top;
 let projects = [
-    {name: 'CLIMBING QUIZ', description: 'This is a brief quiz to test technical knowledge of anchors and knots used in climbing. It includes some of my favorite tidbits of climbing trivia and is geared toward serious climbers and guides.', screenshot: {src: 'screenshots/climbing-quiz-screenshot.png', alt: 'a screenshot of the climbing quiz app'}, live: '', repo: '', color: '#6B839E'}
+    {name: 'Climbing Quiz', description: 'This is a brief quiz to test technical knowledge of anchors and knots used in climbing. It includes some of my favorite tidbits of climbing trivia and is geared toward serious climbers and guides.', technologies: 'This app was made using HTML, CSS, and Javascript.', screenshot: {src: 'screenshots/climbing-quiz-screenshot.png', alt: 'a screenshot of the climbing quiz app'}, live: '', repo: '', color: '#6B839E'}
 ];
 let sections = [$('#projects'), $('#bio'), $('#contact')];
 
+//makes my-name heading stick to the nav bar on scroll
 function stickToTop(){
     if (window.pageYOffset > stickyPosition) {
         myName.addClass("sticky");
@@ -19,29 +20,6 @@ function stickToTop(){
 }
 
 //highlights jump elements in Nav when window scrolls to their corresponding sections on the page
-/*function highlightJump(){
-    if (window.pageYOffset >= $('#projects').offset().top && window.pageYOffset < $('#bio').offset().top){
-        $('.jump.projects').addClass('highlight');
-        $('.jump.bio').removeClass('highlight');
-        $('.jump.contact').removeClass('highlight');
-    }
-    else if (window.pageYOffset >= $('#bio').offset().top && window.pageYOffset < $('#contact').offset().top){
-        $('.jump.bio').addClass('highlight');
-        $('.jump.projects').removeClass('highlight');
-        $('.jump.contact').removeClass('highlight');
-    }
-    else if (window.pageYOffset >= $('#contact').offset().top){
-        $('.jump.contact').addClass('highlight');
-        $('.jump.projects').removeClass('highlight');
-        $('.jump.bio').removeClass('highlight');
-    }
-    else{
-        $('.jump.contact').removeClass('highlight');
-        $('.jump.projects').removeClass('highlight');
-        $('.jump.bio').removeClass('highlight');
-    }
-}*/
-
 function highlightJump(){
     for (let i = 0; i < sections.length; i++){
         if (window.pageYOffset >= sections[i].offset().top - $('nav').outerHeight(true) && window.pageYOffset < sections[i].offset().top + sections[i].outerHeight(true) - $('nav').outerHeight(true)){
@@ -53,21 +31,22 @@ function highlightJump(){
     }
 }
 
+//takes the projects array and displays all projects contained within it
 function generateProjects(){
     for (let i = 0; i < projects.length; i++){
         $('.project-list').append(`
             <li class="project">
-                <h3>${projects[i].name}</h5>
+                <h3>${projects[i].name}</h3>
                 <div class="project-flex-container">
                     <div class = "screenshot-box">
                         <img class="screenshot" src='${projects[i].screenshot.src}' alt='${projects[i].screenshot.alt}'>
                     </div>
                     <div class="description-box">
                         <p class="description">${projects[i].description}</p>
-                        <div class="project-links">
-                            <a href=${projects[i].live}>Live</a>
-                            <a href=${projects[i].live}>Repo</a>
-                        </div>
+                        <p class="description">${projects[i].technologies}</p>
+                        <p class="project-links">
+                            <a href=${projects[i].live}>live</a><a href=${projects[i].live}>repo</a>
+                        </p>
                     </div>
                 </div>
             </li>
@@ -76,6 +55,7 @@ function generateProjects(){
     
 }
 
+//nav functionality, scrolls to position of the corresponding section when a nav button is clicked
 function jumpToPosition(object){
     $("body, html").animate({ 
         scrollTop: $($(object).attr('href')).offset().top - $('nav').outerHeight(true)
